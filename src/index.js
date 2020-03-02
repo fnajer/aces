ff40ed5e5bb872b535ae6e1e5aff41adc94713aa
fnajer <lexactr@gmail.com>
2020-03-02 23:56:43 +0300

    setup project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import App from 'app';
 import * as serviceWorker from './serviceWorker';
+import configureStore from 'redux/configureStore';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css';
+
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+if (process.env.NODE_ENV !== 'production' && module.hot) {
+  module.hot.accept();
+}
