fc344102d0d6814ae75ea0efa04dd96583739fc2
fnajer <lexactr@gmail.com>
2020-03-02 23:57:52 +0300

    add tabs constants

---
+++ src/constants/tabs.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    name: 'Все',
+    routeName: '/all',
+  },
+  {
+    name: 'Активные',
+    routeName: '/active',
+  },
+  {
+    name: 'Удаленные',
+    routeName: '/deleted',
+  }
+]
+
+export const PATHS = {
+  ALL: '/all',
+  ACTIVE: '/active',
+  DELETED: '/deleted',
+}
